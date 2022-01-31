a2d678265f6876bb64e770a947b95ad190eb7455
leosban <leonautcco@gmail.com>
2022-01-31 12:06:44 -0300

    Aula 17

--- src/A0017-type-assertions/A0017-type-assertions.ts
+++ src/A0017-type-assertions/A0017-type-assertions.ts
@@ -7,6 +7,7 @@ const body1 = document.querySelector('body');
 if (body1) body1.style.background = 'red ';
 
 // Non-null assertion (!)
+// eslint-disable-next-line @typescript-eslint/no-non-null-assertion
 const body2 = document.querySelector('body')!;
 if (body2) body2.style.background = 'red ';
 
@@ -24,4 +25,5 @@ input.focus();
 // Não podemos fazer a asserção do elemento HTML como number
 // pois o 'number' não tem a ver com o elemento HTML referido
 // para reverter a situação usamos 'unknown' e depois o definimos como number
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
 const body4 = document.querySelector('body') as unknown as number;
