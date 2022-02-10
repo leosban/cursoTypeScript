1e3a9ea719100fc47fea0fa89e55a6c49f0a165d
leosban <leonautcco@gmail.com>
2022-02-10 16:47:23 -0300

    Aula 61

---
+++ src/A0061-importing-javascript-into-typescript/A0061-importing-javascript-into-typescript.ts
@@ -0,0 +1,8 @@
+// Trabalhando com importação de JS para TS
+// Ir no tsconfig.json e descomentar a opção "allowJs"
+// Para fazer a importação, usamos o seguinte metódo
+import { sum } from './module';
+
+// Uso do type casting para 'number' para não reclamar
+const result = sum(2, 2) as number;
+console.log(result);
