c4151e4871b39b0b74c6667e30b6e2afcbd2003f
leosban <leonautcco@gmail.com>
2022-02-10 16:03:16 -0300

    Aula 60

--- src/A0059-all-decorators/A0059-all-decorators.ts
+++ src/A0059-all-decorators/A0059-all-decorators.ts
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 // Tipo de construtor da classe
 type Constructor = new (...args: any[]) => any;
 
---
+++ src/A0060-creating-a-namespace/A0060-creating-a-namespace.ts
@@ -0,0 +1,34 @@
+/* eslint-disable @typescript-eslint/no-namespace */
+// Namespace é um arquivo ou módulo, que já é nativo no es6, onde podemos importar
+// ou exportar esse módulo
+
+// Só podemos usar as coisas dentro do namespace, se tentar usarmos fora
+// o retorno será undefined, pois fora, ela não existe
+// Para usar para fora, usamos export
+namespace MyNameSpace {
+  // Podemos inserir qualquer coisa que irá funcionar
+  // Para pegar essa váriavel usamos a palavra export, porém não funciona no es6
+  export const nameOfNameSpace = 'Leônidas';
+  // Criação de classe como exemplo
+  export class NameSpacePerson {
+    constructor(public name: string) {}
+  }
+  // Instanciando a classe dentro do namespace
+  const person = new NameSpacePerson('Junior');
+  console.log(person);
+
+  // Para acessar a const abaixo temos que exportar o namespace que já está
+  // dentro de um namespace
+  export namespace OtherNameSpace {
+    export const nameOfNameSpace = 'Name at the other namespace';
+  }
+}
+
+// Instanciando a classe fora do namespace
+const personNameSpace = new MyNameSpace.NameSpacePerson('Junior');
+console.log(personNameSpace);
+console.log(MyNameSpace.nameOfNameSpace);
+console.log(MyNameSpace.OtherNameSpace);
+
+// Metódo para sair do script mode;
+export default 1;
